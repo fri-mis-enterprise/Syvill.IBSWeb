904cf1946ade02ccb56c33c90dbe4485daafbd51
azhadolfo <azhadolfo1@gmail.com>
2026-04-21 14:50:20 +0800

    cleaning v8

---
+++ IBS.DataAccess/Migrations/20260421063836_RemoveFieldV4.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFieldV4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_bienes",
+                table: "services");
+
+            migrationBuilder.DropColumn(
+                name: "is_filpride",
+                table: "services");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_bienes",
+                table: "services",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_filpride",
+                table: "services",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
